1128004d5ed4bcb805651b419184891934bb901f
yajamon <yajamon.tatsuki@gmail.com>
2017-01-12 19:37:28 +0900

    Prompts comment out

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,17 +10,17 @@ module.exports = Generator.extend({
       'Welcome to the divine ' + chalk.red('@yajamon/generator-react-typescript') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    // var prompts = [{
+    //   type: 'confirm',
+    //   name: 'someAnswer',
+    //   message: 'Would you like to enable this option?',
+    //   default: true
+    // }];
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
+    // return this.prompt(prompts).then(function (props) {
+    //   // To access props later use this.props.someAnswer;
+    //   this.props = props;
+    // }.bind(this));
   },
 
   writing: function () {
